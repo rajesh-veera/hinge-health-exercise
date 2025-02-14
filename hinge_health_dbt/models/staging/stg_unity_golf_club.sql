ff6a24dd2308d0bd9a2242747fa201522f8e1f44
Rajesh Veera <rajesh.javiliveera@gmail.com>
2025-02-13 21:03:55 -0800

    stage and intermediate models
    Added Stage and Intermediate models

--- hinge_health_dbt/models/staging/stg_unity_golf_club.sql
+++ hinge_health_dbt/models/staging/stg_unity_golf_club.sql
@@ -2,10 +2,11 @@ SELECT
     id,
     TRIM(first_name) as first_name,
     TRIM(last_name) as last_name,
-    TO_DATE(cast(dob as TEXT), 'YYYY-MM-DD') as dob,
+    TO_DATE(CAST(dob as TEXT), 'YYYY-MM-DD') as dob,
     company_id::int as company_id,
-    TO_DATE(cast(last_active as TEXT), 'YYYY-MM-DD') as last_active,
-    score::int as score,
+    TO_DATE(CAST(last_active as TEXT), 'YYYY-MM-DD') as last_active,
+    score::INT as score,
     member_since::int as member_since,
-    UPPER(SUBSTRING(state, 1, 2)) as state
+    UPPER(SUBSTRING(state, 1, 2)) as state,
+    'unity_golf_club' as source
 FROM {{ source('raw', 'unity_golf_club') }}
--- hinge_health_dbt/models/staging/stg_us_softball_league.sql
+++ hinge_health_dbt/models/staging/stg_us_softball_league.sql
@@ -1,10 +1,12 @@
 SELECT
     id,
-    TRIM(name) as name,
-    TO_DATE(cast(date_of_birth as TEXT), 'YYYY-MM-DD') as date_of_birth,
+    split_part(name, ' ', 1) as first_name,
+    split_part(name, ' ', 2) as last_name,
+    TO_DATE(cast(date_of_birth as TEXT), 'YYYY-MM-DD') as dob,
     company_id::int as company_id,
     TO_DATE(cast(last_active as TEXT), 'YYYY-MM-DD') as last_active,
     score::int as score,
-    joined_league::int as joined_league,
-    {{ state_to_abbreviation('us_state')}} as state_to
+    joined_league::int as member_since,
+    {{ state_to_abbreviation('us_state')}} as state,
+    'us_softball_league' as source
 FROM {{ source('raw', 'us_softball_league') }}
